c0c960e0b8719f79c67ffacb4b570e8c32a84ea8
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-09-22 08:45:28 +0300

    add app container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import CssBaseLine from "@material-ui/core/CssBaseline";
+import {Container, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        padding: 20,
+    },
+});
 
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  )
+    const classes = useStyles();
+
+    return <Router>
+        <CssBaseLine/>
+        <NavBar/>
+        <Container  className={classes.root}>
+            <Switch>
+                <Route path="/">
+                    <Home/>
+                </Route>
+            </Switch>
+        </Container>
+    </Router>
 }
 
 export default App;
