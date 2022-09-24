8c2e685e97aa2bab17e157461d6cf3c2f7a8573f
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-09-24 08:13:39 +0300

    add context

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import {AppBar, Button, IconButton, makeStyles, Toolbar} from "@material-ui/core";
+import React, {useContext} from 'react';
+import {AppBar, IconButton, makeStyles, Toolbar} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import Badge from '@material-ui/core/Badge';
+import { CartContext } from "../contexts/CartContext";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,21 +18,20 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 const NavBar = () => {
-
     const classes = useStyles();
-
+    const { cart } = useContext(CartContext);
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
+                        <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        dakan
+                        sowge
                     </Typography>
-                    <Badge badgeContent={2} color="secondary">
-                    <ShoppingBasketIcon/>
+                    <Badge overlap="rectangular" color='secondary' badgeContent={cart.itemsCount}>
+                        <ShoppingBasketIcon/>
                     </Badge>
                 </Toolbar>
             </AppBar>
