c9ca05541e434e8c7d412a1a356dd79db9a0d39b
Nawaf-Almansour <nawaaf.almansour@gmail.com>
2022-09-24 08:13:56 +0300

    add url

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const appConfig = {
-    apiURL: process.env.REACT_APP_API_URL || 'http://localhost:1337',
+    apiURL: process.env.REACT_APP_API_URL || 'http://localhost:1337/api',
+    URL: process.env.REACT_APP_URL || 'http://localhost:1337',
 };
 
 export { appConfig };
